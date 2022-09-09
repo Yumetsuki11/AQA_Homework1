513057c1987833dc4cb1d42c63ac238878352b33
Yumetsuki11 <shar2107@yandex.ru>
2022-09-09 19:10:24 +0300

    Tests on TestNG added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,38 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+    public CashbackHackService hacker = new CashbackHackService();
+
+    @Test
+    public void shouldReturn0WhenDivisibleBy1000() {
+        Assert.assertEquals(hacker.remain(5000), 0);
+    }
+
+    @Test
+    public void shouldReturn1000When1000() {
+        Assert.assertEquals(hacker.remain(1000), 0);
+    }
+
+    @Test
+    public void shouldReturn999When1() {
+        Assert.assertEquals(hacker.remain(1), 999);
+    }
+
+    @Test
+    public void shouldReturn580When420() {
+        Assert.assertEquals(hacker.remain(420), 580);
+    }
+
+    @Test
+    public void shouldReturn1When999() {
+        Assert.assertEquals(hacker.remain(999), 1);
+    }
+
+    @Test
+    public void shouldReturn499When2501() {
+        Assert.assertEquals(hacker.remain(2501), 499);
+    }
+}
