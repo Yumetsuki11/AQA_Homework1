db2639fb80d5993fb78c84a8ae3e8e26c1d68bab
Yumetsuki11 <shar2107@yandex.ru>
2022-09-09 19:07:45 +0300

    JUnit Platform added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
     public CashbackHackService hacker = new CashbackHackService();
@@ -34,5 +35,35 @@ public class CashbackHackServiceTest {
     @Test
     public void shouldReturn499When2501() {
         Assert.assertEquals(hacker.remain(2501), 499);
+    }
+
+    @Test
+    public void jupiter_ShouldReturn0WhenDivisibleBy1000() {
+        Assertions.assertEquals(hacker.remain(5000), 0);
+    }
+
+    @Test
+    public void jupiter_ShouldReturn1000When1000() {
+        Assertions.assertEquals(hacker.remain(1000), 0);
+    }
+
+    @Test
+    public void jupiter_ShouldReturn999When1() {
+        Assertions.assertEquals(hacker.remain(1), 999);
+    }
+
+    @Test
+    public void jupiter_ShouldReturn580When420() {
+        Assertions.assertEquals(hacker.remain(420), 580);
+    }
+
+    @Test
+    public void jupiter_ShouldReturn1When999() {
+        Assertions.assertEquals(hacker.remain(999), 1);
+    }
+
+    @Test
+    public void jupiter_ShouldReturn499When2501() {
+        Assertions.assertEquals(hacker.remain(2501), 499);
     }
 }
